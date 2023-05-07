7516910e2e908b39912178172449145be842da9e
Khalil <Khalilbentaiba@gmail.com>
2023-05-07 16:09:26 +0100

    recursive fibonacci

--- Fibonacci.js
+++ Fibonacci.js
@@ -5,9 +5,21 @@ function fibonacci(num) {
 
     if (num === 1) return arr;
 
-    for (let i = 2; i < num; i++) arr.push(arr[i-2] + arr[i-1]);
-    
-    return arr
+    for (let i = 2; i < num; i++) arr.push(arr[i - 2] + arr[i - 1]);
+
+    return arr;
 }
 
-console.log(fibonacci(8))
+
+
+function fibonacciRec(num) {
+    if (num === 1) return [0]
+    if (num === 2) return [0, 1]
+    return [
+        ...fibonacciRec(num - 1),
+        fibonacciRec(num - 1)[num-2] + fibonacciRec(num - 1)[num-3]
+    ];
+}
+
+console.log(fibonacci(8));
+console.log(fibonacciRec(8))
