e01ed8c3d1be32d9fd69775822ffbbb3f62da8f8
Khalil <Khalilbentaiba@gmail.com>
2023-05-08 13:53:23 +0100

    add mergeSort function

--- MergeSort.js
+++ MergeSort.js
@@ -1,3 +1,14 @@
+function mergeSort(arr) {
+    if (arr.length < 2) return arr;
+
+    const middle = Math.floor(arr.length / 2);
+
+    const left = mergeSort(arr.slice(0, middle));
+    const right = mergeSort(arr.slice(middle, arr.length));
+
+    return merge(left, right);
+}
+
 function merge(arr1, arr2) {
     const result = [];
     let [i, j] = [0, 0];
@@ -6,9 +17,11 @@ function merge(arr1, arr2) {
         else result.push(arr2[j++]);
     }
 
-    if (arr1.length === i) return [...result, ...arr2.slice(arr1.length)];
-    return [...result, ...arr1.slice(arr2.length)];
+    if (arr1.length === i) return [...result, ...arr2.slice(j)];
+    return [...result, ...arr1.slice(i)];
 }
 
-console.log(merge([1, 5, 9, 20, 25], [3, 4, 8, 19, 24, 55, 100, 150]));
-
+console.log(mergeSort([])); // []
+console.log(mergeSort([5, 2, 18, 19, 3, 1])); // [1, 2, 3, 5, 18, 19]
+console.log(mergeSort([25, 21, 11, 55, 80, 43, 36, 69])); // [11, 21, 25, 36, 43, 55, 69, 80]
+console.log(mergeSort([23, 968, 753, 534, 788, 891, 485, 563])) // [23, 485, 534, 563, 753, 788, 891, 968]
