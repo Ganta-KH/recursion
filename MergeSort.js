377ba4202e76813648681c96780c68d568f17afd
Khalil <Khalilbentaiba@gmail.com>
2023-05-08 12:29:12 +0100

    edit merge function to add the remain of a array

--- MergeSort.js
+++ MergeSort.js
@@ -1,18 +1,14 @@
 function merge(arr1, arr2) {
     const result = [];
-    let i = 0;
-    let j = 0;
+    let [i, j] = [0, 0];
     while (i !== arr1.length && j !== arr2.length) {
-        if (arr1[i] <= arr2[j]) {
-            result.push(arr1[i]);
-            i++;
-        } else {
-            result.push(arr2[j]);
-            j++;
-        }
+        if (arr1[i] <= arr2[j]) result.push(arr1[i++]);
+        else result.push(arr2[j++]);
     }
-    if (arr1.length === i) return [...result, arr2[arr2.length-1]];
-    return [...result, arr1[arr1.length-1]];
+
+    if (arr1.length === i) return [...result, ...arr2.slice(arr1.length)];
+    return [...result, ...arr1.slice(arr2.length)];
 }
 
-console.log(merge([1, 5, 8], [3, 4, 9]));
+console.log(merge([1, 5, 9, 20, 25], [3, 4, 8, 19, 24, 55, 100, 150]));
+
